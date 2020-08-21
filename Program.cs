dc31c3ee33ff8ae96fd9892a4a823b0ef8b31b4e
asia chicon <chiconba@my.erau.edu>
2020-08-20 23:34:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace RussianRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] oddNumbers = new int[19] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37 };
+            int[] evenNumbers = new int[19] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38 };
+            int[] specialNumbers = new int[2] { 0, 00 };
+            string[] colorBoard = new string[] { "black", "red", "green" };
+            Random randomNumber = new Random();
+
+            var r = Random.Next(38);
+
+            while(true)
+            {
+
+            }
+                 
+               
+        }
+    }
+}
